5813da546a713c86b3ca7b5193650581dd730e75
NikolajHK <niko3009@edu.easj.dk>
2020-05-14 11:20:14 +0200

    RelayCommands + Ikke færdig handler, viewmodel og view

---
+++ ZealandRoomBooking/Handler/BookingHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZealandRoomBooking.Model;
+using ZealandRoomBooking.ViewModel;
+
+namespace ZealandRoomBooking.Handler
+{
+    class BookingHandler
+    {
+        public UserViewModel UserViewModel { get; set; }
+        public BookingerViewModel BookingerViewModel { get; set; }
+
+        public BookingHandler(BookingerViewModel bookingerViewModel)
+        {
+            BookingHandler = bookingerViewModel;
+        }
+
+        public void CreateBooking()
+        {
+
+        }
+
+        public async void DeleteBooking()
+        {
+
+        }
+    }
+}
